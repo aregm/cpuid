0b4a207c30582e8949526dc3b27681523fa224ac
Intel Go Team <areg.melik-adamyan@intel.com>
2015-10-08 19:45:41 +0300

    Update cpuidlow_amd64.s
    Removed unnecessary comments

--- cpuidlow_amd64.s
+++ cpuidlow_amd64.s
@@ -1,5 +1,3 @@
-// func Cpuid_eax(arg uint32) (eax, ebx, ecx, edx uint32) 
-
 // func cpuid_low(arg1, arg2 uint32) (eax, ebx, ecx, edx uint32)
 TEXT ·cpuid_low(SB),NOSPLIT,$0-24
     MOVL    arg1+0(FP), AX
