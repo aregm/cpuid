1397c43eb355cd1dd6039749d56661e234683035
Areg Melik-Adamyan <areg.melikadamyan@gmail.com>
2017-03-29 12:02:36 -0500

    Update cpuidlow_amd64.s

--- cpuidlow_amd64.s
+++ cpuidlow_amd64.s
@@ -1,3 +1,6 @@
+// Copyright 2017 Intel Corporation.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 #include "textflag.h"
 
 // func cpuid_low(arg1, arg2 uint32) (eax, ebx, ecx, edx uint32)
